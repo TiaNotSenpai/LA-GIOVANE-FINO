10319f01e155c6eae15524384f4e47297e5411b4
Tia <tianotsenpai@gmail.com>
2025-06-21 20:11:28 +0200

    some changes

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  // Imposta le cartelle di input e output
+  // Leggiamo da /src e scriviamo il sito finito in /_site
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin"); // Copiamo anche la futura cartella admin
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "../_includes" // Le diciamo dove trovare i layout
+    }
+  };
+};
