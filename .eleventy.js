cb846f238b9fd0337cf0322b86d3f56dab2caba6
Tia <tianotsenpai@gmail.com>
2025-06-22 14:46:07 +0200

    used an alias for base

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,28 @@
 module.exports = function(eleventyConfig) {
   
-  // La funzione "Passthrough" è il modo più robusto per copiare
-  // i nostri asset (CSS, JS, Immagini) nella cartella finale del sito.
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/js/");
-  eleventyConfig.addPassthroughCopy("./src/images/");
-
-  // Aggiungiamo la futura cartella per il CMS
-  eleventyConfig.addPassthroughCopy("./src/admin/");
+  // LA SOLUZIONE: "LAYOUT ALIASING"
+  // Diciamo a Eleventy: "Ogni volta che vedi 'layout: base',
+  // usa il file che si trova in 'layouts/base.html'".
+  // Questo elimina ogni possibile confusione con i percorsi.
+  eleventyConfig.addLayoutAlias("base", "layouts/base.html");
   
-  // Questa è la configurazione più esplicita possibile delle directory.
+  // Copia gli asset (CSS, JS, Immagini) nella cartella finale.
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  
+  // La configurazione delle directory, corretta e confermata.
   return {
-    // Specifichiamo che i template che Eleventy deve processare sono questi:
     templateFormats: ["md", "njk", "html"],
-    
-    // Specifichiamo che sia i file Markdown che HTML devono usare
-    // il motore di template Nunjucks (che ci permette di usare {{ variabili }}).
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
 
     dir: {
-      input: "src",          // La nostra cartella sorgente
-      includes: "_includes", // La sottocartella per i layout
-      data: "_data",         // Per dati futuri
-      output: "_site"        // La cartella dove viene costruito il sito finale
+      input: "src",
+      includes: "_includes", // Cerca i layout qui dentro...
+      data: "_data",
+      output: "_site"
     }
   };
 };
