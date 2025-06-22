5c203cb49f9800b00274d4d7064f3f5bb38892e4
Tia <tianotsenpai@gmail.com>
2025-06-22 15:05:58 +0200

    LAST TRY

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,19 @@
 module.exports = function (eleventyConfig) {
-  // Copia le cartelle di asset (CSS, JS, Immagini) nella cartella finale.
+  // Copia le cartelle di asset nella cartella finale.
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
-  
-  // Aggiungi la cartella del CMS quando la useremo.
   eleventyConfig.addPassthroughCopy("src/admin");
 
   // Ritorna la configurazione delle directory.
-  // Questa è la configurazione standard che NON può fallire.
+  // Questa è la configurazione standard di settore.
   return {
     passthroughFileCopy: true,
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes", // La cartella è RELATIVA all'input
+      layouts: "_layouts"    // Specifichiamo anche la cartella dei layout
     },
   };
 };
