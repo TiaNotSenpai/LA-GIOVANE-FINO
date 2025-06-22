95bdb9380513daccd93dd789dec048b36317ad9f
Tia <tianotsenpai@gmail.com>
2025-06-22 15:13:49 +0200

    EDDAJE

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,15 @@
 module.exports = function (eleventyConfig) {
-  // Copia le cartelle di asset nella cartella finale.
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
 
-  // Ritorna la configurazione delle directory.
-  // Questa è la configurazione standard di settore.
   return {
     passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes", // La cartella è RELATIVA all'input
-      layouts: "_layouts"    // Specifichiamo anche la cartella dei layout
+      includes: "_includes", // L'unica cosa che conta per i layout
     },
   };
 };
