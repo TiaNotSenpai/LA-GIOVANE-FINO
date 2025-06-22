f6dbde28be4c51839788e26ea823d2a515cb6df5
Tia <tianotsenpai@gmail.com>
2025-06-22 14:59:47 +0200

    Reverted and tried a new approach

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,22 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  // Copia le cartelle di asset (CSS, JS, Immagini) nella cartella finale.
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
   
-  // Copia gli asset (CSS, JS, Immagini) nella cartella finale.
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/admin");
-  
+  // Aggiungi la cartella del CMS quando la useremo.
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  // Ritorna la configurazione delle directory.
+  // Questa è la configurazione standard che NON può fallire.
   return {
-    templateFormats: ["md", "njk", "html"],
+    passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-
     dir: {
       input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+      includes: "_includes", // La cartella è RELATIVA all'input
+    },
   };
 };
