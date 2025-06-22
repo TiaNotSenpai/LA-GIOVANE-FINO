5c28a936aebd357a86662b845633157a28ebeb6d
Tia <tianotsenpai@gmail.com>
2025-06-22 14:54:14 +0200

    New approach to eleventy for netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,11 @@
 module.exports = function(eleventyConfig) {
   
-  // LA SOLUZIONE: "LAYOUT ALIASING"
-  // Diciamo a Eleventy: "Ogni volta che vedi 'layout: base',
-  // usa il file che si trova in 'layouts/base.html'".
-  // Questo elimina ogni possibile confusione con i percorsi.
-  eleventyConfig.addLayoutAlias("base", "layouts/base.html");
-  
   // Copia gli asset (CSS, JS, Immagini) nella cartella finale.
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/admin");
   
-  // La configurazione delle directory, corretta e confermata.
   return {
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
@@ -20,7 +13,7 @@ module.exports = function(eleventyConfig) {
 
     dir: {
       input: "src",
-      includes: "_includes", // Cerca i layout qui dentro...
+      includes: "_includes",
       data: "_data",
       output: "_site"
     }
